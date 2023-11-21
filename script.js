ae8fc7586683def5a067367654ac233a3c305cd6
Xajper <104763972+webs1te@users.noreply.github.com>
2023-11-21 18:00:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const $counters = document
+    .querySelectorAll('.counter');
+  
+$counters.forEach(($counter) => {
+    $counter.innerText = '0';
+    const updateCounter = () => {
+        const target = Number(
+            $counter.getAttribute('data-target'),
+        );
+        const count = Number($counter.innerText);
+        const increment = target / 200;
+        if (count < target) {
+            $counter.innerText =
+                `${Math.ceil(count + increment)}`;
+            setTimeout(updateCounter, 20);
+        } else {
+            $counter.innerText = target;
+        }
+    };
+  
+    updateCounter();
+});
+
 $(document).ready(function() {
     $(window).scroll(function() {
       if(this.scrollY > 20) {
@@ -61,26 +84,4 @@ $(document).ready(function() {
   
   });
   
-const $counters = document
-    .querySelectorAll('.counter');
-  
-$counters.forEach(($counter) => {
-    $counter.innerText = '0';
-    const updateCounter = () => {
-        const target = Number(
-            $counter.getAttribute('data-target'),
-        );
-        const count = Number($counter.innerText);
-        const increment = target / 200;
-        if (count < target) {
-            $counter.innerText =
-                `${Math.ceil(count + increment)}`;
-            setTimeout(updateCounter, 20);
-        } else {
-            $counter.innerText = target;
-        }
-    };
-  
-    updateCounter();
-});
   
