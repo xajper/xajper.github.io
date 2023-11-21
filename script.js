2c16d2844d4e4fb50cc106f7af21ecf701d896cb
Xajper <104763972+webs1te@users.noreply.github.com>
2023-11-21 12:15:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,86 @@
-const $counters = document
-    .querySelectorAll('.counter');
-
-$counters.forEach(($counter) => {
-    $counter.innerText = '0';
-    const updateCounter = () => {
-        const target = Number(
-            $counter.getAttribute('data-target'),
-        );
-        const count = Number($counter.innerText);
-        const increment = target / 200;
-        if (count < target) {
-            $counter.innerText =
-                `${Math.ceil(count + increment)}`;
-            setTimeout(updateCounter, 20);
-        } else {
-            $counter.innerText = target;
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if(this.scrollY > 20) {
+        $('.navbar').addClass("sticky");
+      } else {
+        $('.navbar').removeClass("sticky");
+      }
+      if(this.scrollY > 500) {
+        $('.scroll-up-btn').addClass("show");
+      } else {
+        $('.scroll-up-btn').removeClass("show");
+      }
+    })
+  
+    $('.scroll-up-btn').click(function() {
+      $('html').animate({scrollTop: 0});
+    })
+    
+    $('.menu-btn').click(function() {
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+    })
+  
+    var typed = new Typed('.typing', {
+      strings: [
+        "łatwy w użyciu",
+        "stworzony z miłości",
+        "fajny",
+        "uniwersalny",
+        "wielo funkcyjny",
+        "nowoczesny",
+      ],
+      typeSpeed: 120,
+      backSpeed: 80,
+      loop: true,
+    })
+  
+    // this is really cool
+    // but you should vote Mikuni first
+  
+    $('.carousel').owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+        0:{
+          items: 1,
+          nav: false
+        },
+        600:{
+          items: 2,
+          nav: false
+        },
+        1000:{
+          items: 3,
+          nav: false
         }
-    };
-
-    updateCounter();
-});
+      }
+    });
+  
+  });
+  
+  const $counters = document
+      .querySelectorAll('.counter');
+  
+  $counters.forEach(($counter) => {
+      $counter.innerText = '0';
+      const updateCounter = () => {
+          const target = Number(
+              $counter.getAttribute('data-target'),
+          );
+          const count = Number($counter.innerText);
+          const increment = target / 200;
+          if (count < target) {
+              $counter.innerText =
+                  `${Math.ceil(count + increment)}`;
+              setTimeout(updateCounter, 20);
+          } else {
+              $counter.innerText = target;
+          }
+      };
+  
+      updateCounter();
+  });
+  
