0523f2d5d378b663ad4f846e4d7cdc539cd86542
Xajper <104763972+webs1te@users.noreply.github.com>
2023-11-21 17:55:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -61,26 +61,26 @@ $(document).ready(function() {
   
   });
   
-  const $counters = document
-      .querySelectorAll('.counter');
+const $counters = document
+    .querySelectorAll('.counter');
   
-  $counters.forEach(($counter) => {
-      $counter.innerText = '0';
-      const updateCounter = () => {
-          const target = Number(
-              $counter.getAttribute('data-target'),
-          );
-          const count = Number($counter.innerText);
-          const increment = target / 200;
-          if (count < target) {
-              $counter.innerText =
-                  `${Math.ceil(count + increment)}`;
-              setTimeout(updateCounter, 20);
-          } else {
-              $counter.innerText = target;
-          }
-      };
+$counters.forEach(($counter) => {
+    $counter.innerText = '0';
+    const updateCounter = () => {
+        const target = Number(
+            $counter.getAttribute('data-target'),
+        );
+        const count = Number($counter.innerText);
+        const increment = target / 200;
+        if (count < target) {
+            $counter.innerText =
+                `${Math.ceil(count + increment)}`;
+            setTimeout(updateCounter, 20);
+        } else {
+            $counter.innerText = target;
+        }
+    };
   
-      updateCounter();
-  });
+    updateCounter();
+});
   
