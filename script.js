04d0295109761568d2946ca715f88f2f6c2659db
Xajper <104763972+webs1te@users.noreply.github.com>
2023-11-24 20:52:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,69 +29,67 @@ function celebrate() {
     // Dodaj emotki twarzy z konfetii
     const celebrationContainer = document.querySelector('.containersub');
     celebrationContainer.innerHTML += '<div class="emoji">🎉😊🎉</div>';
-};
+}
 
 $(document).ready(function() {
     $(window).scroll(function() {
-      if(this.scrollY > 20) {
-        $('.navbar').addClass("sticky");
-      } else {
-        $('.navbar').removeClass("sticky");
-      }
-      if(this.scrollY > 500) {
-        $('.scroll-up-btn').addClass("show");
-      } else {
-        $('.scroll-up-btn').removeClass("show");
-      }
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
     })
-  
+
     $('.scroll-up-btn').click(function() {
-      $('html').animate({scrollTop: 0});
+        $('html').animate({scrollTop: 0});
     })
-    
+
     $('.menu-btn').click(function() {
-      $('.navbar .menu').toggleClass("active");
-      $('.menu-btn i').toggleClass("active");
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     })
-  
+
     var typed = new Typed('.typing', {
-      strings: [
-        "łatwy w użyciu",
-        "stworzony z miłości",
-        "fajny",
-        "uniwersalny",
-        "wielo funkcyjny",
-        "nowoczesny",
-      ],
-      typeSpeed: 120,
-      backSpeed: 80,
-      loop: true,
+        strings: [
+            "łatwy w użyciu",
+            "stworzony z miłości",
+            "fajny",
+            "uniwersalny",
+            "wielo funkcjonalny",
+            "nowoczesny",
+        ],
+        typeSpeed: 120,
+        backSpeed: 80,
+        loop: true,
     })
-  
+
     // this is really cool
     // but you should vote Mikuni first
-  
+
     $('.carousel').owlCarousel({
-      margin: 20,
-      loop: true,
-      autoplayTimeOut: 2000,
-      autoplayHoverPause: true,
-      responsive: {
-        0:{
-          items: 1,
-          nav: false
-        },
-        600:{
-          items: 2,
-          nav: false
-        },
-        1000:{
-          items: 3,
-          nav: false
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
         }
-      }
     });
-  
-  });
-  
-  
+
+});
