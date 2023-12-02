c4ebbe51f4edfd2bdc3c65ffa392f75c60b997ed
Xajper <104763972+webs1te@users.noreply.github.com>
2023-12-02 11:59:57 +0100

    Update scriptmapy.js

--- scriptmapy.js
+++ scriptmapy.js
@@ -262,17 +262,14 @@ function filterArticles() {
 }
 
 const allArticles = [
-  {
-    title: "Party Activities",
-    content: "Witaj! Mapa którą obecnie widzisz jest zbiorem różnych minigier..."
-
-  },
-  {
-    title: "Clicker Evolved",
-    content: "Cześć! Przychodzę do was z kolejną mapą pt. Clicker Evolved..."
-
-  },
-
+    {
+        title: "Party Activities",
+        content: "Witaj! Mapa którą obecnie widzisz jest zbiorem różnych minigier..."
+    },
+    {
+        title: "Clicker Evolved",
+        content: "Cześć! Przychodzę do was z kolejną mapą pt. Clicker Evolved..."
+    },
 ];
 
 const articles = allArticles;
