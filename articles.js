92bb08c61ac5c6a9df87b75674b40fe368581cbd
Xajper <104763972+webs1te@users.noreply.github.com>
2023-12-02 11:43:37 +0100

    Update articles.js

--- articles.js
+++ articles.js
@@ -1,1 +1,30 @@
+const articles = [
+    {
+        title: "Party Activities",
+        content: "Witaj! Mapa którą obecnie widzisz jest zbiorem różnych minigier...",
+        author: "Xajper",
+        downloads: 0
+    },
+    {
+        title: "Clicker Evolved",
+        content: "Cześć! Przychodzę do was z kolejną mapą pt. Clicker Evolved...",
+        author: "Xajper",
+        downloads: 0
+    },
+    {
+        title: "Ostatnia Twierdza",
+        content: "...",
+        author: "Xajper",
+        downloads: 0
+    }
+];
+
+const storedArticles = JSON.parse(localStorage.getItem('articles'));
+
+
+const allArticles = storedArticles || articles;
+
+function saveToLocalStorage() {
+    localStorage.setItem('articles', JSON.stringify(allArticles));
+}
 
