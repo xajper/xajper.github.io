fe095776b9faa9da7990f3f1742d139969cc06dd
Xajper <104763972+webs1te@users.noreply.github.com>
2023-12-02 10:32:57 +0100

    Update scriptomnie.js

--- scriptomnie.js
+++ scriptomnie.js
@@ -1,1 +1,30 @@
+const $counters = document.querySelectorAll('.counter');
 
+$counters.forEach(($counter) => {
+    $counter.innerText = '0';
+    const updateCounter = () => {
+        const target = Number($counter.getAttribute('data-target'));
+        const count = Number($counter.innerText);
+        const increment = target / 200;
+        if (count < target) {
+            $counter.innerText = `${Math.ceil(count + increment)}`;
+            setTimeout(updateCounter, 20);
+        } else {
+            $counter.innerText = target;
+
+            if (target >= 1000) {
+                celebrate();
+            }
+        }
+    };
+    updateCounter();
+});
+
+function celebrate() {
+    const confettiSettings = { target: 'my-canvas' };
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+
+    const celebrationContainer = document.querySelector('.containersub');
+    celebrationContainer.innerHTML += '<div class="emoji">🎉</div>';
+}
