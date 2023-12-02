3cd31d5911b19711b839acb8a86bdc43c584be74
Xajper <104763972+webs1te@users.noreply.github.com>
2023-12-02 11:09:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+let downloadCounts = {};
+
+app.use(express.json());
+
+app.get('/download', (req, res) => {
+    const { url } = req.query;
+
+    if (!downloadCounts[url]) {
+        downloadCounts[url] = 1;
+    } else {
+        downloadCounts[url]++;
+    }
+
+    res.json({ count: downloadCounts[url] });
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Serwer działa na http://localhost:${port}`);
+});
