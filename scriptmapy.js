e397749605beef6406a22336f9781667e235a12f
Xajper <104763972+webs1te@users.noreply.github.com>
2023-12-02 11:55:22 +0100

    Update scriptmapy.js

--- scriptmapy.js
+++ scriptmapy.js
@@ -261,6 +261,20 @@ function filterArticles() {
     });
 }
 
+const allArticles = [
+  {
+    title: "Party Activities",
+    content: "Witaj! Mapa którą obecnie widzisz jest zbiorem różnych minigier..."
+
+  },
+  {
+    title: "Clicker Evolved",
+    content: "Cześć! Przychodzę do was z kolejną mapą pt. Clicker Evolved..."
+
+  },
+
+];
+
 const articles = allArticles;
 
 articles.forEach((article, index) => {
