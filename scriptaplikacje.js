d0d1e678125cfcc8aa918c0f301a19fb444b43cb
Xajper <104763972+webs1te@users.noreply.github.com>
2023-12-25 19:09:55 +0100

    Update scriptaplikacje.js

--- scriptaplikacje.js
+++ scriptaplikacje.js
@@ -1,10 +1,3 @@
-function animateTile(tile) {
-    tile.classList.add('clicked');
-    setTimeout(function () {
-        tile.classList.remove('clicked');
-    }, 300);
-}
-
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -14,12 +7,23 @@ function getRandomColor() {
     return color;
 }
 
+// Funkcja do animowania kafelka
 function animateTile(element) {
     element.classList.add('clicked');
 
+    // Usuń poprzednią klasę 'random-color' i dodaj nowy losowy kolor
+    element.classList.remove('random-color');
     element.style.backgroundColor = getRandomColor();
 
     setTimeout(function () {
         element.classList.remove('clicked');
     }, 300);
 }
+
+// Przypisz klasę 'random-color' do każdego kafelka przy ładowaniu strony
+document.addEventListener('DOMContentLoaded', function () {
+    var tiles = document.getElementsByClassName('tile');
+    for (var i = 0; i < tiles.length; i++) {
+        tiles[i].classList.add('random-color');
+    }
+});
