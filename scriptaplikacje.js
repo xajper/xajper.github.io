67ec89832b347819e91fe854adcf9c97daf73f18
Xajper <104763972+webs1te@users.noreply.github.com>
2023-12-25 19:07:18 +0100

    Update scriptaplikacje.js

--- scriptaplikacje.js
+++ scriptaplikacje.js
@@ -4,3 +4,22 @@ function animateTile(tile) {
         tile.classList.remove('clicked');
     }, 300);
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function animateTile(element) {
+    element.classList.add('clicked');
+
+    element.style.backgroundColor = getRandomColor();
+
+    setTimeout(function () {
+        element.classList.remove('clicked');
+    }, 300);
+}
