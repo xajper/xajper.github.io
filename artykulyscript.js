84c78e86cf19c224d641baf55b75f0bdef24bdd8
Xajper <104763972+webs1te@users.noreply.github.com>
2024-02-26 16:12:31 +0100

    Update artykulyscript.js

--- artykulyscript.js
+++ artykulyscript.js
@@ -111,16 +111,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function blokujMysz(event) {
     if (event.button === 2 || event.which === 3) {
-
-      event.preventDefault();
+        event.preventDefault();
     }
 }
-  
+
+function blokujKlawisze(event) {
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+
+    if (event.ctrlKey && event.key === 'u') {
+        event.preventDefault();
+    }
+}
+
 document.addEventListener('contextmenu', function (event) {
     event.preventDefault();
 });
-  
+
 document.addEventListener('mousedown', blokujMysz);
+
+document.addEventListener('keydown', blokujKlawisze);
 
 var subscribersDB = firebase.database().ref("subscribers");
   
