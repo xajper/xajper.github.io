e0163fd10092ac4f3be065d36fc79c4b6228640e
Xajper <104763972+webs1te@users.noreply.github.com>
2024-02-26 16:26:15 +0100

    Update scriptomnie.js

--- scriptomnie.js
+++ scriptomnie.js
@@ -2,17 +2,27 @@ const $counters = document.querySelectorAll('.counter');
 
 function blokujMysz(event) {
     if (event.button === 2 || event.which === 3) {
-
-      event.preventDefault();
+        event.preventDefault();
     }
-  }
-  
+}
+
+function blokujKlawisze(event) {
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+
+    if (event.ctrlKey && event.key === 'u') {
+        event.preventDefault();
+    }
+}
+
 document.addEventListener('contextmenu', function (event) {
     event.preventDefault();
 });
-  
+
 document.addEventListener('mousedown', blokujMysz);
 
+document.addEventListener('keydown', blokujKlawisze);
   
 function celebrate() {
     const confettiSettings = { target: 'my-canvas' };
