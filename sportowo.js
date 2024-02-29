916d639053982afb1c5ab4752358fe632b00c001
Xajper <104763972+webs1te@users.noreply.github.com>
2024-02-29 15:40:45 +0100

    Update sportowo.js

--- sportowo.js
+++ sportowo.js
@@ -109,6 +109,30 @@ document.addEventListener('DOMContentLoaded', function () {
     getArticlesCount();
 });
 
+function blokujMysz(event) {
+    if (event.button === 2 || event.which === 3) {
+        event.preventDefault();
+    }
+}
+
+function blokujKlawisze(event) {
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+
+    if (event.ctrlKey && event.key === 'u') {
+        event.preventDefault();
+    }
+}
+
+document.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+});
+
+document.addEventListener('mousedown', blokujMysz);
+
+document.addEventListener('keydown', blokujKlawisze);
+
 var subscribersDB = firebase.database().ref("subscribers");
   
 document.getElementById("subscribersForm").addEventListener("submit", submitForm);
