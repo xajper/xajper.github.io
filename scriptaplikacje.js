820500a3b625b6082b425e029c4d4cba12c316b7
Xajper <104763972+xajper@users.noreply.github.com>
2024-07-20 15:08:00 +0200

    Update scriptaplikacje.js

--- scriptaplikacje.js
+++ scriptaplikacje.js
@@ -1,5 +1,5 @@
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
+    var letters = '0123456789ABCDE';
     var color = '#';
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
